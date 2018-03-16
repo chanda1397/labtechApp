7f53a2c7c555578978c968d0d18a31a646d38eaa
brittany <bolivthom@outlook.com>
2018-03-16 13:10:51 -0500

    changes made

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-<<<<<<< HEAD
-from flask import Flask
-=======
 from config import *
->>>>>>> 03ce13ecfcf5bbccc76bd5beee48813fe04584d1
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "password"
@@ -13,12 +9,9 @@ app.config['SECRET_KEY'] = "password"
 db = SQLAlchemy(app)
 
 
-<<<<<<< HEAD
-from src import views
 app.config.from_object(__name__)
-=======
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = DEBUG
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = SECRET_KEY
 
@@ -33,4 +26,3 @@ login_manager = LoginManager(app)
 login_manager.login_view = "auth.login"
 
 from src import views
->>>>>>> 03ce13ecfcf5bbccc76bd5beee48813fe04584d1
--- src/views.py
+++ src/views.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-from src import app
-from flask import render_template, request, redirect, url_for, flash
-from flask_login import login_user, logout_user, current_user, login_required
-from forms import MyForm
-
-
-=======
 from flask import render_template
 from src import app, login_manager
 from src.auth import auth
@@ -20,7 +12,6 @@ app.register_blueprint(auth, url_prefix='/auth')
 def load_user(user_id):
     return LabTech.query.get(int(user_id))
 
->>>>>>> 03ce13ecfcf5bbccc76bd5beee48813fe04584d1
 
 @app.route('/')
 
@@ -35,7 +26,6 @@ def cisco():
     """Render website's cisco page."""
     return render_template('cisco.html')
 
-<<<<<<< HEAD
 @app.route('/profile', methods=['GET', 'POST'])
 @app.route('/cisco/keyboard')
 def ciscoKeyboard():
@@ -48,8 +38,6 @@ def ciscoKeyboard():
         functional = form.functional.data
         
     return render_template('ciscoKeyboard.html',  form = form)
-=======
->>>>>>> 03ce13ecfcf5bbccc76bd5beee48813fe04584d1
 
 @app.route('/faculty')
 @login_required
@@ -64,23 +52,12 @@ def engineering():
     """Render website's engineering page."""
     return render_template('engineering.html')
 
-<<<<<<< HEAD
 @app.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port="8080")
-=======
->>>>>>> 03ce13ecfcf5bbccc76bd5beee48813fe04584d1
 
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
     print error
     return "lol", 404
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port="8080")
